c1ebbdf3c7387a8d374462fc6a4015b70b650081
Matt Robinson <matt@puppetlabs.com>
2011-07-19 22:34:03 -0700

    more prolog exercises

---
+++ prolog/authors.pl
@@ -0,0 +1,4 @@
+author_book(jules_verne, from_the_earth_to_the_moon).
+author_book(jules_verne, twenty_thousand_leagues_under_the_sea).
+author_book(charles_stross, accelerando).
+author_book(ken_grimwood, replay).
